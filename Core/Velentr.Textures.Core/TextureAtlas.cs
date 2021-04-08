71463c30bb898c140b1b3a9cecc21f76048f0728
Christian Webber <ricky.webber@hotmail.com>
2021-04-07 22:02:34 -0400

    WIP

---
+++ Core/Velentr.Textures.Core/TextureAtlas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Velentr.Input
+{
+    internal class TextureAtlas
+    {
+
+        internal Texture2D Texture;
+
+        public TextureAtlas(TextureManager manager, int width, int height, SurfaceFormat surfaceFormat)
+        {
+            Manager = manager;
+            Width = width;
+            Height = height;
+            SurfaceFormat = surfaceFormat;
+
+            Texture = new Texture2D(manager.GraphicsDevice, Width, Height, false, SurfaceFormat);
+        }
+
+        public TextureManager Manager { get; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public bool Full { get; protected set; }
+
+        public SurfaceFormat SurfaceFormat { get; }
+
+        public bool AddTextureToAtlas(string name, Texture2D texture, out TextureInfo info)
+        {
+
+        }
+
+
+    }
+}
---
+++ Core/Velentr.Textures.Core/TextureInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Velentr.Input
+{
+    public class TextureInfo
+    {
+
+        public TextureInfo(string name, TextureAtlas atlas)
+        {
+            Name = name;
+
+            Atlas = atlas;
+        }
+
+        public string Name { get; }
+
+        public TextureAtlas Atlas { get; }
+    }
+}
